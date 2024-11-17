bdab83a368e8c895a6d15c6d5e3582ac4d9884e5
Sarath C Ani <mailsforsarath98@gmail.com>
2024-11-17 12:55:12 +0530

    feat: Refactor SeederService to streamline category and inventory seeding process

--- src/main/java/com/example/spring_security_demo/service/SeederService.java
+++ src/main/java/com/example/spring_security_demo/service/SeederService.java
@@ -24,198 +24,93 @@ public class SeederService {
     private InventoryRepository inventoryRepository;
 
     public void seed() {
-        Category electronics = new Category("Electronics", null);
+        // Categories
+        Category electronics = new Category(1, "Electronics", null);
+        Category laptop = new Category(2, "Laptop", electronics);
+        Category mobile = new Category(3, "Mobile", electronics);
+
         categoryRepository.save(electronics);
-
-        Category sports = new Category("Sports", null);
-        categoryRepository.save(sports);
-
-        Category laptop = new Category("Laptop", electronics);
         categoryRepository.save(laptop);
-
-        Filter brandLaptop = new Filter("Brand", laptop);
-        FilterOption apple = new FilterOption("Apple", brandLaptop);
-        FilterOption lenovo = new FilterOption("Lenovo", brandLaptop);
-        FilterOption dell = new FilterOption("Dell", brandLaptop);
-
-        filterOptionRepository.save(apple);
-        filterOptionRepository.save(lenovo);
-        filterOptionRepository.save(dell);
-
-
-
-        Filter ramLaptop = new Filter("RAM", laptop);
-        FilterOption ram16GB = new FilterOption("16 GB", ramLaptop);
-        FilterOption ram8GB = new FilterOption("8 GB", ramLaptop);
-        FilterOption ram4GB = new FilterOption("4 GB", ramLaptop);
-
-        filterOptionRepository.save(ram16GB);
-        filterOptionRepository.save(ram8GB);
-        filterOptionRepository.save(ram4GB);
-
-        Filter screenSize = new Filter("Screen Size", laptop);
-        FilterOption inch16 = new FilterOption("16 inch", screenSize);
-        FilterOption inch14 = new FilterOption("14 inch", screenSize);
-        FilterOption inch13 = new FilterOption("13 inch", screenSize);
-
-        filterOptionRepository.save(inch16);
-        filterOptionRepository.save(inch14);
-        filterOptionRepository.save(inch13);
-
-
-
-        Filter ssd = new Filter("SSD", laptop);
-        FilterOption ssd512 = new FilterOption("512", ssd);
-        FilterOption ssd256 = new FilterOption("256", ssd);
-        FilterOption ssd128 = new FilterOption("128", ssd);
-
-        filterOptionRepository.save(ssd512);
-        filterOptionRepository.save(ssd256);
-        filterOptionRepository.save(ssd128);
-
-
-
-        filterRepository.save(ramLaptop);
-        filterRepository.save(screenSize);
-        filterRepository.save(ssd);
-        filterRepository.save(brandLaptop);
-
-        Inventory macbook16 = new Inventory("macbook16","M2 chip inside", 100, 1500, 0.2f, laptop);
-        inventoryRepository.save(macbook16);
-
-        FilterValues macbook16brand = new FilterValues(apple,macbook16);
-        filterValuesRepository.save(macbook16brand);
-
-        FilterValues macbook16ram = new FilterValues(ram8GB,macbook16);
-        filterValuesRepository.save(macbook16ram);
-
-        FilterValues macbook16ssd = new FilterValues(ssd256,macbook16);
-        filterValuesRepository.save(macbook16ssd);
-
-        FilterValues macbook16screenSize = new FilterValues(inch14,macbook16);
-        filterValuesRepository.save(macbook16screenSize);
-
-
-        Inventory lenovoIdealPad = new Inventory("lenovo IdealPad","Intel Core i9 7th Gen chip inside", 90, 1000, 0.3f, laptop);
-        inventoryRepository.save(lenovoIdealPad);
-
-        FilterValues idealPadbrand = new FilterValues(lenovo,lenovoIdealPad);
-        filterValuesRepository.save(idealPadbrand);
-
-        FilterValues idealPadram = new FilterValues(ram16GB,lenovoIdealPad);
-        filterValuesRepository.save(idealPadram);
-
-        FilterValues idealPadssd = new FilterValues(ssd512,lenovoIdealPad);
-        filterValuesRepository.save(idealPadssd);
-
-        FilterValues idealPadscreenSize = new FilterValues(inch13,lenovoIdealPad);
-        filterValuesRepository.save(idealPadscreenSize);
-
-
-        Inventory dellInspiron = new Inventory("Dell Inspiron","Intel Core i9 9th Gen chip inside", 80, 900, 0.1f, laptop);
-        inventoryRepository.save(dellInspiron);
-
-        FilterValues inspironBrand = new FilterValues(dell,dellInspiron);
-        filterValuesRepository.save(inspironBrand);
-
-        FilterValues inspironRam = new FilterValues(ram4GB,dellInspiron);
-        filterValuesRepository.save(inspironRam);
-
-        FilterValues inspironSsd = new FilterValues(ssd128,dellInspiron);
-        filterValuesRepository.save(inspironSsd);
-
-        FilterValues inspironScreenSize = new FilterValues(inch16,dellInspiron);
-        filterValuesRepository.save(inspironScreenSize);
-
-
-
-        Category mobile = new Category("Mobile", electronics);
         categoryRepository.save(mobile);
 
-        Filter ramPhone = new Filter("RAM", mobile);
-        FilterOption ramPhone8GB = new FilterOption("8 GB", ramPhone);
-        FilterOption ramPhone12GB = new FilterOption("12 GB", ramPhone);
-        FilterOption ramPhone16GB = new FilterOption("16 GB", ramPhone);
+        // Filters
+        Filter screenSizeLaptop = new Filter(1, "Screen size", laptop);
+        Filter ramLaptop = new Filter(2, "RAM", laptop);
+        Filter colorLaptop = new Filter(3, "Color", laptop);
 
-        filterOptionRepository.save(ramPhone8GB);
-        filterOptionRepository.save(ramPhone12GB);
-        filterOptionRepository.save(ramPhone16GB);
+        Filter screenSizeMobile = new Filter(4, "Screen size", mobile);
+        Filter ramMobile = new Filter(5, "RAM", mobile);
 
-        Filter storage = new Filter("ROM", mobile);
-        FilterOption rom128GB = new FilterOption("128 GB", storage);
-        FilterOption rom256GB = new FilterOption("256 GB", storage);
-        FilterOption rom512GB = new FilterOption("512 GB", storage);
+        filterRepository.save(screenSizeLaptop);
+        filterRepository.save(ramLaptop);
+        filterRepository.save(colorLaptop);
+        filterRepository.save(screenSizeMobile);
+        filterRepository.save(ramMobile);
 
-        filterOptionRepository.save(rom128GB);
-        filterOptionRepository.save(rom256GB);
-        filterOptionRepository.save(rom512GB);
+        // Filter Options
+        // Laptop Filter Options
+        FilterOption laptop16Inch = new FilterOption(1, "16 inch", screenSizeLaptop);
+        FilterOption laptop14Inch = new FilterOption(8, "14 inch", screenSizeLaptop);
 
-        Filter brandMobile = new Filter("Brand", mobile);
-        FilterOption Apple = new FilterOption("Apple", brandMobile);
-        FilterOption samsung = new FilterOption("Samsung", brandMobile);
-        FilterOption onePlus = new FilterOption("one Plus", brandMobile);
+        FilterOption ram16GBLaptop = new FilterOption(2, "16 GB", ramLaptop);
+        FilterOption ram8GBLaptop = new FilterOption(9, "8 GB", ramLaptop);
 
-        filterOptionRepository.save(Apple);
-        filterOptionRepository.save(samsung);
-        filterOptionRepository.save(onePlus);
+        FilterOption spaceGray = new FilterOption(3, "Space Gray", colorLaptop);
+        FilterOption white = new FilterOption(4, "White", colorLaptop);
+        FilterOption silver = new FilterOption(5, "Silver", colorLaptop);
 
+        filterOptionRepository.save(laptop16Inch);
+        filterOptionRepository.save(laptop14Inch);
+        filterOptionRepository.save(ram16GBLaptop);
+        filterOptionRepository.save(ram8GBLaptop);
+        filterOptionRepository.save(spaceGray);
+        filterOptionRepository.save(white);
+        filterOptionRepository.save(silver);
 
-        filterRepository.save(ramPhone);
-        filterRepository.save(storage);
-        filterRepository.save(brandMobile);
+        // Mobile Filter Options
+        FilterOption mobile8Inch = new FilterOption(6, "8 inch", screenSizeMobile);
+        FilterOption mobile12Inch = new FilterOption(10, "12 inch", screenSizeMobile);
 
+        FilterOption ram8GBMobile = new FilterOption(7, "8 GB", ramMobile);
+        FilterOption ram6GBMobile = new FilterOption(11, "6 GB", ramMobile);
 
-        Inventory iPhone15 = new Inventory("IPhone 15","All new IPhone 15 with Inbuild AI Assistance", 50, 1000, 0.1f, mobile);
-        inventoryRepository.save(iPhone15);
+        filterOptionRepository.save(mobile8Inch);
+        filterOptionRepository.save(mobile12Inch);
+        filterOptionRepository.save(ram8GBMobile);
+        filterOptionRepository.save(ram6GBMobile);
 
-        FilterValues phoneBrand = new FilterValues(Apple,iPhone15);
-        filterValuesRepository.save(phoneBrand);
+        // Inventories
+        Inventory macbookPro16 = new Inventory(1, "Macbook Pro 16", "High-performance laptop", 10, 2500, 10, laptop);
+        Inventory macbookAir14 = new Inventory(3, "Macbook Air 14", "Lightweight and portable laptop", 15, 1500, 5, laptop);
+        Inventory dellXPS13 = new Inventory(4, "Dell XPS 13", "Compact high-performance laptop", 8, 1800, 8, laptop);
 
-        FilterValues iPhoneRam = new FilterValues(ramPhone16GB,iPhone15);
-        filterValuesRepository.save(iPhoneRam);
+        Inventory iPhone16 = new Inventory(2, "iPhone 16", "Latest iPhone model", 20, 1200, 5, mobile);
+        Inventory samsungGalaxyS22 = new Inventory(5, "Samsung Galaxy S22", "Flagship Android mobile", 25, 1100, 10, mobile);
 
-        FilterValues iPhoneRom = new FilterValues(rom512GB,iPhone15);
-        filterValuesRepository.save(iPhoneRam);
+        inventoryRepository.save(macbookPro16);
+        inventoryRepository.save(macbookAir14);
+        inventoryRepository.save(dellXPS13);
+        inventoryRepository.save(iPhone16);
+        inventoryRepository.save(samsungGalaxyS22);
 
+        // Filter Values for Laptops
+        filterValuesRepository.save(new FilterValues(1, macbookPro16, laptop16Inch)); // 16 inch screen
+        filterValuesRepository.save(new FilterValues(2, macbookPro16, ram16GBLaptop)); // 16 GB RAM
+        filterValuesRepository.save(new FilterValues(3, macbookPro16, spaceGray)); // Space Gray color
 
+        filterValuesRepository.save(new FilterValues(4, macbookAir14, laptop14Inch)); // 14 inch screen
+        filterValuesRepository.save(new FilterValues(5, macbookAir14, ram8GBLaptop)); // 8 GB RAM
+        filterValuesRepository.save(new FilterValues(6, macbookAir14, white)); // White color
 
-        Inventory samsungS24 = new Inventory("Samsung S24","Samsung S24 with 120X Zoom", 60, 1200, 0.1f, mobile);
-        inventoryRepository.save(samsungS24);
+        filterValuesRepository.save(new FilterValues(7, dellXPS13, laptop14Inch)); // 14 inch screen
+        filterValuesRepository.save(new FilterValues(8, dellXPS13, ram8GBLaptop)); // 8 GB RAM
+        filterValuesRepository.save(new FilterValues(9, dellXPS13, silver)); // Silver color
 
-        FilterValues s24Brand = new FilterValues(samsung,samsungS24);
-        filterValuesRepository.save(s24Brand);
+        // Filter Values for Mobiles
+        filterValuesRepository.save(new FilterValues(10, iPhone16, mobile8Inch)); // 8 inch screen
+        filterValuesRepository.save(new FilterValues(11, iPhone16, ram8GBMobile)); // 8 GB RAM
 
-        FilterValues s24Ram = new FilterValues(ramPhone12GB,samsungS24);
-        filterValuesRepository.save(s24Ram);
-
-        FilterValues s24Rom = new FilterValues(rom512GB,samsungS24);
-        filterValuesRepository.save(s24Rom);
-
-
-        Inventory onePlus12 = new Inventory("One Plus 12","One Plus 12 with 108MP camera ", 70, 800, 0.1f, mobile);
-        inventoryRepository.save(onePlus12);
-
-        FilterValues onePlus12Brand = new FilterValues(onePlus,onePlus12);
-        filterValuesRepository.save(onePlus12Brand);
-
-        FilterValues onePlusRam = new FilterValues(ramPhone8GB,onePlus12);
-        filterValuesRepository.save(onePlusRam);
-
-        FilterValues onePlusRom = new FilterValues(rom128GB,onePlus12);
-        filterValuesRepository.save(onePlusRom);
-
-
-
-        Category shoes = new Category("Shoes", sports);
-        categoryRepository.save(shoes);
-
-        Filter brandShoe = new Filter("Brand", shoes);
-        Filter sizeShoe = new Filter("Size", shoes);
-        Filter typeShoe = new Filter("Type", shoes);
-
-        filterRepository.save(brandShoe);
-        filterRepository.save(sizeShoe);
-        filterRepository.save(typeShoe);
-
+        filterValuesRepository.save(new FilterValues(12, samsungGalaxyS22, mobile12Inch)); // 12 inch screen
+        filterValuesRepository.save(new FilterValues(13, samsungGalaxyS22, ram6GBMobile)); // 6 GB RAM
     }
 }
